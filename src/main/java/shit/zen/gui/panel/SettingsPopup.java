cceac8d97d205bf2095e2403abf481493f60343e
Koe <KoeNotPrism@proton.me>
2026-05-31 16:03:50 +0800

    Add ArrayList and List imports to SettingsPopup.java

--- src/main/java/shit/zen/gui/panel/SettingsPopup.java
+++ src/main/java/shit/zen/gui/panel/SettingsPopup.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.client.gui.GuiGraphics;
 import shit.zen.ClientBase;
 import shit.zen.ZenClient;
